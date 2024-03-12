9f766063064c3027649794db14bc06bdad18ca75
wakura-mbuya <wakura9@gmail.com>
2024-03-12 21:41:19 +0300

    Python Loops - task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str_ = ""
+    for i in range(len(str)):
+        if str[i] == n:
+            pass
+        else:
+            str_ += str[i]
+    return (str)
