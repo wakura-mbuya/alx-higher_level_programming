062a1daa1bbdf3a72b43350c7ae3115a3c0797f3
wakura-mbuya <wakura9@gmail.com>
2024-03-13 18:36:29 +0300

    Python Hello world - Task 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-err_msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(err_msg)
+def write_to_stderr(error_msg):
+    """
+    Writes to stderr and exits with status value 1
+    """
+
+    sys.stderr.write(error_msg)
+    sys.stderr.write("\n")
+    sys.exit(1)
+
+error_msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+write_to_stderr(error_msg)
