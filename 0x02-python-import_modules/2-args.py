fafed8c3023f60ec17d2bd94d7e77b6b4aed9b3c
wakura-mbuya <wakura9@gmail.com>
2024-03-14 21:37:55 +0300

    Python Modules - task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) < 1:
+    if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
