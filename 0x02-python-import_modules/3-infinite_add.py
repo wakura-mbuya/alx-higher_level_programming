028326fcdd6a6046aa58cd79cdf24b69ce5e457a
wakura-mbuya <wakura9@gmail.com>
2024-03-14 21:43:57 +0300

    Python Modules - task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     total = 0
     for i in range(1, len(argv)):
-            total += int(argv[i])
+        total += int(argv[i])
     print("{}".format(total))
