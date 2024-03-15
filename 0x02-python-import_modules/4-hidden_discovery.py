6728002b13c4fd65702a85eef94ce81a77fac2d3
wakura-mbuya <wakura9@gmail.com>
2024-03-15 04:40:04 +0300

    Python Modules - Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in names:
-        if "--" not in i:
-            print(i)
+    filtered_names = [name for name in names in not name.startswith("__")]
+    for name in filtered_names:
+        print(name)
