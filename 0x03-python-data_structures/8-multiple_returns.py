0b6f0d91e6948d1287aea7359f51820dc33dc458
wakura-mbuya <wakura9@gmail.com>
2024-03-17 19:57:37 +0300

    Python Lists & Tuples - Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 def multiple_returns(sentence):
-    if len(sentence):
-        return len(sentence), sentence[0]
+    length = len(sentence)
+    if length:
+        return (length, sentence[0])
     else:
-        return len(sentence), None
+        return (length, None)
