1814ab366dfd0f9b311bd595e34cca1d7c038058
wakura-mbuya <wakura9@gmail.com>
2024-03-17 20:05:30 +0300

    Python Lists & Tuples - Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if my_list:
+        max_int = my_list[0]
+        for i in my_list:
+            if i > max_int:
+                max_int = i
+        return max_int
+    return None
