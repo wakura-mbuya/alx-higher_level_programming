436d8e83c7bac43020b608b5376aa4909deb7b16
wakura-mbuya <wakura9@gmail.com>
2024-03-17 18:47:47 +0300

    Python Lists & Tuples - Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     str_ = ""
     for i in range(len(my_string)):
         x = my_string[i]
-        if x != 'c' or x != 'C':
-            str_ = str_ + str(x)
+        if x != 'c' and x != 'C':
+            str_ += str(x)
     return str_
