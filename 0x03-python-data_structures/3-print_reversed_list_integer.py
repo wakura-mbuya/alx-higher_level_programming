82ca9e4f63f2b048628a895eaa533fd71dbc1440
wakura-mbuya <wakura9@gmail.com>
2024-03-17 18:31:16 +0300

    Python Lists & Tuples - Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list - 1, -1, 1)):
-        if i < 0:
-            return
-        else:
-            print("{:d}".format(my_list[i]))
+    while (my_list):
+        print("{:d}".format(my_list.pop()))
