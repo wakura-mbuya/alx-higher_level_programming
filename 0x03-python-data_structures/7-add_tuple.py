5e6922ed972dd819219bdbceff82f4a7cdedf2b5
wakura-mbuya <wakura9@gmail.com>
2024-03-17 19:47:14 +0300

    Python Lists & Tuples - Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # get the values in tuple a
+    if a:
+        a1 = a[0]
+    else:
+        a1 = 0
+
+    if len(a) > 1:
+        a2 = a[1]
+    else:
+        a2 = 0
+
+    # get the values for tuple b
+    if b:
+        b1 = b[0]
+    else:
+        b1 = 0
+
+    if len(b) > 1:
+        b2 = b[1]
+    else:
+        b2 =0
+
+    return (a1 + b1, b2 + a2)
