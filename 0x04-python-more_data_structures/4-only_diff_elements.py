d76571b243fd88aeb98c4162954c9c724826e5ed
wakura-mbuya <wakura9@gmail.com>
2024-03-21 02:39:48 +0300

    Python Set & Dict - Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.difference(set_2)
+    all_elements = set_1.union(set_2)
+    common_elements = set_1.intersection(set_2)
+    return all_elements.difference(common_elements)
