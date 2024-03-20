a0eaa5a245cac1002a97eb2cdc64a3558c208004
wakura-mbuya <wakura9@gmail.com>
2024-03-21 01:55:56 +0300

    Python Set & Dict - Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+def search_replace(my_list, search, replace):i
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
