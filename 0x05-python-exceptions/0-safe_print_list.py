9ad7c2754d83d991492b10c77ef24a3af7e8ffd6
wakura-mbuya <wakura9@gmail.com>
2024-04-09 02:15:08 +0300

    Python Exceptions - Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while (i < x):
-            print(my_list[i], end="");
+            print(my_list[i], end="")
             i += 1
         print()
         return x
